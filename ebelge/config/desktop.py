80832a35b47a5b2c2df09a1aaaf8404209a20d71
Murathan KILIÇ <murathan@vivayazilim.com>
2025-06-05 12:30:44 +0000

    Initial commit for ebelge module

---
+++ ebelge/config/desktop.py
@@ -0,0 +1,12 @@
+from frappe import _
+
+def get_data():
+    return [
+        {
+            "module_name": "E-Belge",
+            "color": "#2ecc71",
+            "icon": "octicon octicon-file-text",
+            "type": "module",
+            "label": _("E-Belge")
+        }
+    ]
---
+++ ebelge/hooks.py
@@ -0,0 +1,13 @@
+from __future__ import unicode_literals
+
+app_name = "ebelge"
+app_title = "E-Belge Yönetimi"
+app_publisher = "Viva Yazılım"
+app_description = "Elektronik belge yönetim sistemi"
+app_icon = "octicon octicon-file-text"
+app_color = "#2ecc71"
+app_email = "murathan@vivayazilim.com"
+app_license = "MIT"
+
+# Desk Menu
+app_include_css = "/assets/ebelge/css/custom.css"
